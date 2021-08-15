376447e7acf33faf14e879551226a5a4bc15f255
WiredSpast <wiredspast@gmail.com>
2021-08-15 13:22:02 +0200

    bugfix writetoconsole + create packet by hash

--- test/test.js
+++ test/test.js
@@ -19,3 +19,32 @@ ext.interceptByNameOrHash(HDirection.TOCLIENT, 'Users', hMessage => {
         console.log(user.getName());
     }
 });
+
+ext.interceptByNameOrHash(HDirection.TOCLIENT, 'Chat', hMessage => {
+    console.log("chat");
+});
+
+ext.interceptByNameOrHash(HDirection.TOCLIENT, 'Objects', hMessage => {
+    let objects = HFloorItem.parse(hMessage.getPacket());
+    console.log("a");
+    ext.writeToConsole(objects.length + " floor items found!");
+    ext.sendToServer(new HPacket('Chat', HDirection.TOCLIENT)
+        .appendInt(-1)
+        .appendString(objects.length + " floor items found!")
+        .appendInt(0)
+        .appendInt(30)
+        .appendInt(0)
+        .appendInt(-1));
+});
+
+ext.interceptByNameOrHash(HDirection.TOCLIENT, 'Items', hMessage => {
+    let items = HWallItem.parse(hMessage.getPacket());
+    ext.writeToConsole(items.length + " wall items found!");
+    ext.sendToServer(new HPacket('Chat', HDirection.TOCLIENT)
+        .appendInt(-1)
+        .appendString(items.length + " wall items found!")
+        .appendInt(0)
+        .appendInt(30)
+        .appendInt(0)
+        .appendInt(-1));
+});

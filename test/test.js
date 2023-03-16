dcf5cb6f63525c0e31e23dc3ce4c402ef7265337
WiredSpast <wiredspast@gmail.com>
2023-03-16 15:03:41 +0100

    Corrected return types

--- test/test.js
+++ test/test.js
@@ -1,13 +1,3 @@
-import { Extension, GInventory, HDirection } from '../index.js';
-import { readFile } from 'fs/promises';
+import { FurniDataUtils, Hotel } from '../index.js';
 
-const extensionInfo = JSON.parse(
-    await readFile(
-        new URL('./package.json', import.meta.url)
-    )
-);
-
-const ext = new Extension(extensionInfo);
-ext.run();
-
-//new GInventory(ext, (items) => console.log(items));
+FurniDataUtils.fetch(Hotel.COM).then(a => console.log(a));
